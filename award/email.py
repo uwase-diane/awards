d1f23b95e4d855132d5fa6ed47efd798515d2121
uwase-diane <diane.uwase13@gmail.com>
2021-01-19 14:08:05 -0500

    welcome email file

---
+++ award/email.py
@@ -0,0 +1,14 @@
+from django.template.loader import render_to_string
+
+def send_welcome_email(name,receiver):
+    # Creating message subject and sender
+    subject = 'Welcome'
+    sender = 'diane.uwase13@gmail.com'
+
+    #passing in the context vairables
+    text_content = render_to_string('email/newsemail.txt',{"name": name})
+    html_content = render_to_string('email/newsemail.html',{"name": name})
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
