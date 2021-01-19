4f9e1517a19493f43ece4ee21d880dcce2eecd37
uwase-diane <diane.uwase13@gmail.com>
2021-01-19 14:07:07 -0500

    admin site

--- award/admin.py
+++ award/admin.py
@@ -1,3 +1,10 @@
+
 from django.contrib import admin
+from.models import Project, Profile, Comments, Rating
+
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Comments)
+admin.site.register(Rating)
