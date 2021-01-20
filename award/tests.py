f4ca1cdaf70767c035e447834d933ef27f781cd6
uwase-diane <diane.uwase13@gmail.com>
2021-01-20 12:30:19 -0500

    added tests

--- award/tests.py
+++ award/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
+
+class TestProfile(TestCase):
+    def setUp(self):
+
+        self.profile_test = Profile(id = 1, name = 'image', profile_picture = 'books.png', bio = 'happy new year', user = self.user)
+
+        def test_instance(self):s
+            self.assertTrue(isinstance(self.profile_test, Profile))
+
+        def test_save_profile(self):
+            self.profile_test.save_profile()
+            profiles = Profile.objects.all()
+            self.assertTrue(len(after) > 0)
+
